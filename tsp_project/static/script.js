cae18f8f2f6e885f9c0159af0e1cfbb3d1a5c847
Kushagra Dasila <dasilakushagra@gmai.com>
2022-05-20 09:42:26 +0530

    new commit

--- tsp_project/static/script.js
+++ tsp_project/static/script.js
@@ -9,11 +9,21 @@ var pt = e.latlng; //event returns lat lng of clicked point
 //Do your related operation here
 });
 // -122.42,37.78;-122.45,37.91;-122.48,37.73
-document.getElementById('submit-btn').addEventListener('click',findDistanceMatrix);
+// document.getElementById('submit-btn').addEventListener('click',findDistanceMatrix);
 
-async function findDistanceMatrix(){
+
+/* async function findDistanceMatrix(){
     var link="https://api.mapbox.com/directions-matrix/v1/mapbox/driving/"+loc.slice(0,loc.length-1)+"?approaches=curb;curb;curb;curb&access_token=pk.eyJ1Ijoia3VzaDc4IiwiYSI6ImNsMzVqZHF3djBmMWMza3A1c2MzY3Y1NjkifQ.-PI6HfCVSnQVi5MguiQISQ";
     let response = await fetch(link);
     let data = await response.json();
     console.log(data);
-}
+} */
+
+// ======= sending data to flask============
+const submitBtn=document.getElementById('submit-btn')
+submitBtn.addEventListener('click',async function(){
+        console.log('clicked')
+        const res=await fetch(`http://127.0.0.1:5000/api?loc=${loc}`)
+        const data=await res.json()
+        console.log(data)
+})
--- tsp_project/tsp.py
+++ tsp_project/tsp.py
@@ -1,4 +1,8 @@
+from statistics import mode
 from flask import Flask, send_from_directory, render_template
+from flask import request
+import requests
+ 
 app = Flask(__name__)
 
 @app.route("/static/<path:path>")
@@ -9,11 +13,27 @@ def static_dir(path):
 def index():
   return render_template('index.html')
 
-@app.route('/my-link/')
-def my_link():
-  print ('I got clicked!')
+@app.route('/api')
+def abc():
+  data = request.args.get('loc')
+  findDistMatrix(data)
+  return {
+        "user": "John Doe",
+    }
 
-  return 'Click.'
+def findDistMatrix(cord,mode="driving"):
+  URL = "https://api.mapbox.com/directions-matrix/v1/mapbox/driving/"+cord[:-1]+"?approaches=curb;curb;curb;curb&access_token=pk.eyJ1Ijoia3VzaDc4IiwiYSI6ImNsMzVqZHF3djBmMWMza3A1c2MzY3Y1NjkifQ.-PI6HfCVSnQVi5MguiQISQ"
+
+  # sending get request and saving the response as response object
+  r = requests.get(url = URL) 
+  
+  # extracting data in json format
+  data = r.json()
+  print(data['durations'])
+  return data
+
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
